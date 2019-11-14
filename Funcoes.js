f7f363c4104084241bb84c381d4d3253ca9ea9a7
Henqieu Coutinho <henriquecoutinho@cpejr.com.br>
2019-11-14 16:51:41 -0300

    Javascript

---
+++ Funcoes.js
@@ -0,0 +1,14 @@
+function pageScroll1() { 
+  console.log("test");
+    window.scrollTo(0,1000);
+  } 
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function MyFunction2() {
+  document.getElementById("myDropdown").classList.toggle("hide")
+}
